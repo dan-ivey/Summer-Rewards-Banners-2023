94a354a6a08a2902ffcb7b3710732e454c2f17fc
Dan Ivey <95397173+dan-ivey@users.noreply.github.com>
2023-04-27 13:18:54 -0700

    CREATED RESIZE FOLDERS + BUILT 320X50

---
+++ app/320x50/scripts/Animation.js
@@ -0,0 +1,69 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var animationWindow = document.getElementById('animationWindow');
+	var animData = {
+        wrapper: animationWindow,
+        animType: 'svg',
+        loop: false,
+        prerender: true,
+        autoplay: false,
+        path: './scripts/data.json',
+        assetsPath: './images/'
+    };
+
+    var anim = bodymovin.loadAnimation(animData);
+
+	var cta = document.getElementById('cta');
+    var buttonExit = document.getElementById('button-exit');
+    var tl1 = new TimelineMax({paused:true});
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+
+	t.to(cta, .5, {delay:5, scale:1.06, opacity:1, ease:Sine.easeOut});
+	t.to(cta, .5, {delay:5.5, scale:1, opacity:1, onComplete:buttonGo, ease:Sine.easeIn});
+		tl1.to(cta, .25, {scale:1.06, force3D:false, ease:Sine.easeOut})
+		.to(cta, .25, {scale:1, force3D:false, ease:Sine.easeIn});
+
+		function buttonGo() {
+			buttonExit.addEventListener('mouseover', function () {
+				tl1.play(0);
+			});
+			
+		}
+
+		
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+		anim.play();
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
